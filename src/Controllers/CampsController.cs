eef30cba34d2d127ed8d24cbed40c7ee1f344528
Olya Wysocky <x-OlyaW@provokesolutions.com>
2019-06-12 10:04:53 -0700

    add firt route

---
+++ src/Controllers/CampsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CoreCodeCamp.Controllers
+{
+   [Route("api")]
+   public class CampsController : ControllerBase
+    {
+        public object Get()
+        {
+            return new { Moniker = "ATL2019", Name = "Code Camp" };
+        }
+    }
+}
